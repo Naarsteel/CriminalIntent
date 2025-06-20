ac4ce64a701e910e1e3122a238ba7b6cd5b9c766
Naarsteel <re.nation@yandex.ru>
2025-06-20 17:17:23 +0700

    Реализация интерфейса обратных вызовов (CrimeFragment.kt)

--- app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
@@ -13,6 +13,7 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import java.util.Date
 import java.util.UUID
 
 private const val TAG = "CrimeFragment"
@@ -20,7 +21,7 @@ private const val ARG_CRIME_ID = "crime_id"
 private const val DIALOG_DATE = "DialogDate"
 private const val REQUEST_DATE = 0
 
-class CrimeFragment : Fragment() {
+class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
@@ -106,6 +107,11 @@ class CrimeFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
+    }
+
+    override fun onDateSelected(date: Date) {
+        crime.date = date
+        updateUI()
     }
 
     private fun updateUI() {
