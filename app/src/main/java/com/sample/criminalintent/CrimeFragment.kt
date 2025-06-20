a64da9512b505adf91bfed56c5b40c43384cad1c
Naarsteel <re.nation@yandex.ru>
2025-06-20 16:05:34 +0700

    Функция newInstance(UUID) (CrimeFragment.kt)

--- app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
@@ -10,6 +10,9 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import java.util.UUID
+
+private const val ARG_CRIME_ID = "crime_id"
 
 class CrimeFragment : Fragment() {
 
@@ -68,6 +71,17 @@ class CrimeFragment : Fragment() {
         solvedCheckBox.apply {
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance(crimeId: UUID): CrimeFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_CRIME_ID, crimeId)
+            }
+            return CrimeFragment().apply {
+                arguments = args
             }
         }
     }
