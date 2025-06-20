0587b756188b4ffb278da1b9b26809188a1f990f
Naarsteel <re.nation@yandex.ru>
2025-06-20 16:00:10 +0700

    Реализация обратных вызовов (MainActivity.kt)

--- app/src/main/java/com/sample/criminalintent/MainActivity.kt
+++ app/src/main/java/com/sample/criminalintent/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.sample.criminalintent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.util.UUID
 
+private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +22,9 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.fragment_container, fragment)
                 .commit()
         }
+    }
+
+    override fun onCrimeSelected(crimeId: UUID) {
+        Log.d(TAG, "MainActivity.onCrimeSelected: $crimeId")
     }
 }
