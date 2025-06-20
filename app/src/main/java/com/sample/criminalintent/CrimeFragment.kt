34f9fd3e54e129c53d562105d295572a7547f85e
Naarsteel <re.nation@yandex.ru>
2025-06-20 12:46:49 +0700

    Переопределение Fragment.onCreate(Bundle?) (CrimeFragment.kt)

--- app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeFragment.kt
@@ -1,6 +1,13 @@
 package com.sample.criminalintent
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 class CrimeFragment : Fragment() {
+
+    private lateinit var crime: Crime
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
 }
