ef09e3b8a42c72213c3754d2ae8660bc24c8d6ac
Naarsteel <re.nation@yandex.ru>
2025-06-20 15:38:04 +0700

    Удаления ссылки на старую версию ViewModel (CrimeListFragment.kt)

--- app/src/main/java/com/sample/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeListFragment.kt
@@ -24,11 +24,6 @@ class CrimeListFragment : Fragment() {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -40,13 +35,10 @@ class CrimeListFragment : Fragment() {
             view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        updateUI()
-
         return view
     }
 
-    private fun updateUI() {
-        val crimes = crimeListViewModel.crimes
+    private fun updateUI(crimes: List<Crime>) {
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
