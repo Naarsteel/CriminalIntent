9722b2ac98d384d792102aedaae4293a032e8772
Naarsteel <re.nation@yandex.ru>
2025-06-20 17:25:25 +0700

    Заполнение ресурса меню (CrimeListFragment.kt)

--- app/src/main/java/com/sample/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeListFragment.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -70,6 +72,11 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
     }
 
     private fun updateUI(crimes: List<Crime>) {
