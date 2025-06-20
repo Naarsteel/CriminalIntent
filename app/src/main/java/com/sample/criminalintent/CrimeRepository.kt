2e8d2f970830dde51df222082801806abc30e7a2
Naarsteel <re.nation@yandex.ru>
2025-06-20 15:15:29 +0700

    Настройка свойств репозитория (CrimeRepository.kt)

--- app/src/main/java/com/sample/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/sample/criminalintent/CrimeRepository.kt
@@ -1,8 +1,20 @@
 package com.sample.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import com.sample.criminalintent.database.CrimeDatabase
+
+private const val DATABASE_NAME = "crime-database"
 
 class CrimeRepository private constructor(context: Context) {
+
+    private val database : CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+    private val crimeDao = database.crimeDao()
+
     companion object {
         private var INSTANCE: CrimeRepository? = null
         fun initialize(context: Context) {
